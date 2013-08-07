ed43b2e55432a34df9adef882b48ea111ba49b6d
Kaoru NAKAMURA <kaorun55@gmail.com>
2013-08-07 17:35:12 +0900

    コードの順番を解説に合わせて変更した

--- samples/Touchless/Touchless/main.cpp
+++ samples/Touchless/Touchless/main.cpp
@@ -10,50 +10,35 @@ public:
     {
         // アプリケーションがバックグラウンドでもLeapからのフレームデータを受け取れるようにする
         controller.setPolicyFlags( Leap::Controller::PolicyFlag::POLICY_BACKGROUND_FRAMES );
-        controller.enableGesture( Leap::Gesture::Type::TYPE_CIRCLE );
 
         // スクリーンの幅と高さを取得する
         auto screen = controller.locatedScreens()[0];
         windowWidth = screen.widthPixels();
         windowHeight = screen.heightPixels();
 
-        // 円ジェスチャーの認識半径を広げる
-        Leap::Config config = controller.config();
-        std::cout << "Gesture.Circle.MinRadius: " << config.getFloat( "Gesture.Circle.MinRadius" ) << std::endl;
+        // 円ジェスチャーの認識を有効にする
+        controller.enableGesture( Leap::Gesture::Type::TYPE_CIRCLE );
 
-        config.setFloat( "Gesture.Circle.MinRadius", 20 );
-        config.save();
-
+        // タッチ入力の初期化を行う
         TouchInput::Initialize();
     }
 
     // フレーム更新処理
     virtual void onFrame(const Leap::Controller& controller )
     {
+        // 新しいフレームを取得する
         auto frame = controller.frame();
 
-        ////// スタートメニューの部分だけ何とかしたい
-        for ( auto g : frame.gestures() ) {
-            if ( g.hands()[0].fingers().count() == 2 ) {
-                if ( g.type() == Leap::Gesture::Type::TYPE_CIRCLE ) {
-                    if( g.state() == Leap::Gesture::State::STATE_STOP ) {
-                        keybd_event( VK_LWIN, 0, 0, 0 );
-                        keybd_event( VK_LWIN, 0, KEYEVENTF_KEYUP, 0 );
-                        break;
-                    }
-                }
-            }
-        }
-        //////
-
+        // ポインタを取得する
         Leap::PointableList pointables = frame.pointables();
         if ( pointables.empty() ) {
             return;
         }
 
-        // 最初のポインタを使う
+        // 最初のポインタの位置をタッチの位置とする
         auto& pointable = pointables[0];
-        Leap::Vector normalizedPosition = frame.interactionBox().normalizePoint( pointable.stabilizedTipPosition() );
+        Leap::Vector normalizedPosition = frame.interactionBox()
+                                            .normalizePoint( pointable.stabilizedTipPosition() );
         int x = (int)min( normalizedPosition.x * windowWidth, windowWidth - 1 );
         int y = (int)min( windowHeight - normalizedPosition.y * windowHeight, windowHeight - 1 );
 
@@ -69,6 +54,16 @@ public:
         // タッチしてない
         else {
             touch.up();
+        }
+
+        // 円のジェスチャーで Windows キーを押し、スタートメニューに戻る
+        for ( auto g : frame.gestures() ) {
+            if ( (g.type() == Leap::Gesture::Type::TYPE_CIRCLE) &&
+                 (g.state() == Leap::Gesture::State::STATE_STOP) ) {
+                keybd_event( VK_LWIN, 0, 0, 0 );
+                keybd_event( VK_LWIN, 0, KEYEVENTF_KEYUP, 0 );
+                break;
+            }
         }
     }
 
