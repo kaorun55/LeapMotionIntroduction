8cf09a522a2edebd745f215c0f54a211ff7c60b8
Kaoru NAKAMURA <kaorun55@gmail.com>
2013-08-10 07:52:26 +0900

    FrameDataの取得について動作確認した

--- samples/GettingFrameData/GettingFrameData/main.cpp
+++ samples/GettingFrameData/GettingFrameData/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 #include <Leap.h>
 
+void GettingFramesByPolling()
+{
+  Leap::Controller leap;
+
+  int64_t lastFrame = 0;
+  while ( 1 ) {
+    auto frame = leap.frame();
+    if ( !leap.isConnected() ) {
+      std::cout << "leap is not connected." << std::endl;
+      continue;
+    }
+
+    bool focus = leap.hasFocus();
+    if ( !focus ) {
+      std::cout << "application is not focus." << std::endl;
+      continue;
+    }
+
+    auto currentFrameid = frame.id();
+    if ( currentFrameid == lastFrame ) {
+      //std::cout << "processed frame id." << std::endl;
+      continue;
+    }
+
+    //std::cout << "frame count : " << (currentFrameid - lastFrame) << std::endl;
+
+    lastFrame = currentFrameid;
+    
+    // 指の位置の平均値を計算する
+    int count = 0;
+    Leap::Vector average = Leap::Vector();
+    Leap::Finger fingerToAverage = frame.fingers()[0];
+    for( int i = 0; i < 10; i++ ) {
+        // フレーム履歴と指のIDで、過去の指の位置を取得する
+        Leap::Finger fingerFromFrame = leap.frame(i).finger(fingerToAverage.id());
+        if( fingerFromFrame.isValid() ) {
+            average += fingerFromFrame.tipPosition();
+            count++;
+        }
+    }
+    average /= count;
+    std::cout << "finger point : " << average << std::endl;
+  }
+}
+
+
 class SampleListrener : public Leap::Listener
 {
 public:
@@ -14,32 +60,6 @@ public:
     }
 
 };
-
-void GettingFramesByPolling()
-{
-  Leap::Controller leap;
-
-  int64_t lastFrame = 0;
-  while ( 1 ) {
-    auto frame = leap.frame();
-    if ( !leap.isConnected() ) {
-      std::cout << "leap is not connected." << std::endl;
-      continue;
-    }
-
-    if ( !leap.hasFocus() ) {
-      std::cout << "application is not focus." << std::endl;
-      continue;
-    }
-
-    auto currentFrameid = frame.id();
-    if ( currentFrameid == lastFrame ) {
-      continue;
-    }
-
-    lastFrame = currentFrameid;
-  }
-}
 
 void GettingFramesWithCallbacks()
 {
@@ -55,7 +75,7 @@ void GettingFramesWithCallbacks()
 
 void main()
 {
-#if 0
+#if 1
   // ポーリング
   GettingFramesByPolling();
 #else
